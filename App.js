7b6de14961ffa838b57b9e418c0b047d599c5f0b
kal-J <kharljay15@gmail.com>
2020-04-24 03:39:21 +0300

    add App icon

--- App.js
+++ App.js
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from "react";
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import * as Font from "expo-font";
-import { View, ActivityIndicator } from "react-native";
-import HomeScreenRouter from "./screens/HomeScreenRouter";
-import GeneralStatusBar from "./components/GeneralStatusBar";
-import colors from "./layouts/colors";
-
+import React, {useEffect, useState} from 'react';
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
+import * as Font from 'expo-font';
+import {View, ActivityIndicator} from 'react-native';
+import HomeScreenRouter from './screens/HomeScreenRouter';
+import GeneralStatusBar from './components/GeneralStatusBar';
+import colors from './layouts/colors';
 
 const App = () => {
   const [isReady, setIsReady] = useState(false);
   useEffect(() => {
     (async function() {
       await Font.loadAsync({
-        Roboto_medium: require("./assets/fonts/Roboto-Medium.ttf")
+        Roboto_medium: require('./assets/fonts/Roboto-Medium.ttf'),
       });
       setIsReady(true);
     })();
   });
 
   return (
-    <Provider store={ store } >
-      <View style={{ flex: 1 }}>
-        <GeneralStatusBar backgroundColor={colors.primary} barStyle="light-content" />
+    <Provider store={store}>
+      <GeneralStatusBar
+        backgroundColor={colors.primary}
+      />
 
+      <View style={{flex: 1}}>
         {isReady ? <HomeScreenRouter /> : <ActivityIndicator />}
       </View>
     </Provider>
-      
   );
 };
 
--- components/GeneralStatusBar.js
+++ components/GeneralStatusBar.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import { View, StyleSheet, Platform, StatusBar } from "react-native";
+import React, {useEffect} from 'react';
+import {View, StyleSheet, Platform, StatusBar} from 'react-native';
 import colors from '../layouts/colors';
 
-const STATUSBAR_HEIGHT = Platform.OS === "ios" ? 20 : StatusBar.currentHeight;
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
 const styles = StyleSheet.create({
   statusBar: {
     height: STATUSBAR_HEIGHT,
-  }
+    backgroundColor: colors.primary,
+  },
 });
 
-const GeneralStatusBar = (props) => (
-  <View style={styles.statusBar}>
-    <StatusBar translucent  {...props} />
-  </View>
-);
+const GeneralStatusBar = props => {
+  useEffect(() => {
+    StatusBar.setBackgroundColor(colors.primary);
+  }, []);
+
+  return (
+    <View style={styles.statusBar}>
+      <StatusBar barStyle="light-content" translucent={true} {...props} />
+    </View>
+  );
+};
 
 export default GeneralStatusBar;

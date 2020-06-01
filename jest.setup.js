b5b26568e72c066bba0af4a9e963e92352b1ceaa
kal-J <kharljay15@gmail.com>
2020-06-01 05:50:06 +0300

    test: fix breaking react-image-picker module

--- jest.setup.js
+++ jest.setup.js
@@ -2,11 +2,12 @@ import {NativeModules} from 'react-native';
 import jest from 'jest';
 
 // Mock the ImagePickerManager native module to allow us to unit test the JavaScript code
-NativeModules.ImagePickerManager = {
-  showImagePicker: jest.fn(),
-  launchCamera: jest.fn(),
-  launchImageLibrary: jest.fn(),
-};
+// NativeModules.ImagePickerManager = {
+//   showImagePicker: jest.fn(),
+//   launchCamera: jest.fn(),
+//   launchImageLibrary: jest.fn(),
+// };
+//jest.mock('react-native-image-picker', () => ({}));
 
 // Reset the mocks before each test
 global.beforeEach(() => {
